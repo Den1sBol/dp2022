326d1be304872f4f24ab37f2a413b249174613a7
Den1sBol <90184621+Den1sBol@users.noreply.github.com>
2022-06-11 11:17:43 +0300

    Add files via upload

---
+++ lab5/sample/src/main/java/lab5/repositories/userRepository.java
@@ -0,0 +1,13 @@
+package lab5.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import lab5.entity.User;
+
+public interface userRepository extends JpaRepository<User, Integer> {
+
+}
